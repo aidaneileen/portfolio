184ec7e8235a4f95b734dcf0d91d5fb3a6f1cd6d
straysheep14 <aidaneileen123@gmail.com>
2025-04-19 09:20:09 -0700

    Nav bar FIX

--- global.js
+++ global.js
@@ -9,6 +9,7 @@ let pages = [
   { url: 'projects/', title: 'Projects' },
   { url: 'resume/', title: 'Resume' },
   { url: 'contact/', title: 'Contact' },
+  { url: 'https://github.com/aidaneileen', title: 'GitHub' },
 ];
 
 let nav = document.createElement('nav');
